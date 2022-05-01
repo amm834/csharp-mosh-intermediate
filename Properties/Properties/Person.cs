1a50a8f7693c1a4c9eac68357a672d9756eafb52
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-02 00:44:06 +0630

    Properties done

---
+++ Properties/Properties/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public Person(DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+
+        public DateTime BirthDate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var timespan = DateTime.Now - BirthDate;
+                return timespan.Days / 365;
+            }
+        }
+    }
+}
