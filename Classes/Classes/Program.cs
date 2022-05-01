df5b64ac13c1b4c4194cc28734a0d7c308272137
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-01 22:48:31 +0630

    Basic Classes

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Classes
+{
+    class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hello {0}, I'm {1}", to, Name);
+        }
+
+        public static Person Parse(string name)
+        {
+            var person = new Person();
+            person.Name = name;
+            return person;
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = Person.Parse("Aung Aung");
+            person.Introduce("Mg Mg");
+        }
+    }
+}
