18d291fd4c8e04c3b591473439417ab3d32f940a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-01 23:58:57 +0630

    Fields

---
+++ Fields/Fields/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>();
+
+
+        public Customer()
+        {
+        }
+
+        public Customer(int id)
+        {
+            Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+
+        public void Promote()
+        {
+        }
+    }
+}
