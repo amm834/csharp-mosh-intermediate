c054606713eb6be69d5aedddef496e2f366b3a53
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-02 00:20:34 +0630

    Access modifier

---
+++ AccessModifiers/AccessModifiers/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _dateTime;
+
+        public void SetDateTime(DateTime dateTime)
+        {
+            _dateTime = dateTime;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return _dateTime;
+        }
+    }
+}
