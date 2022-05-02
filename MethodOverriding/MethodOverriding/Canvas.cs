a0fa32ad95a6ce0fd8ed91b706e9e4c6773687ee
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-02 19:29:19 +0630

    Method overloading done

---
+++ MethodOverriding/MethodOverriding/Canvas.cs
@@ -0,0 +1,12 @@
+﻿namespace MethodOverriding;
+
+public class Canvas
+{
+    public void DrawShapes(List<Shape> shapes)
+    {
+        foreach (var shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
---
+++ MethodOverriding/MethodOverriding/Shap.cs
@@ -0,0 +1,28 @@
+﻿namespace MethodOverriding;
+
+public class Shape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public virtual void Draw()
+    {
+        Console.WriteLine("Base class drawing");
+    }
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Circle is drawing");
+    }
+}
+
+public class Triangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Triangle is drawing");
+    }
+}
