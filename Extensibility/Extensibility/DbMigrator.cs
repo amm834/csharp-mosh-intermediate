8672558a7d44ac70552116885f5e6d64d0baeb93
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-04 00:34:13 +0630

    Interface extensibility  and OP principle

---
+++ Extensibility/Extensibility/DbMigrator.cs
@@ -0,0 +1,17 @@
+﻿namespace Extensibility;
+
+public class DbMigrator
+{
+    private readonly ILogger _logger;
+
+    public DbMigrator(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void Migrate()
+    {
+        _logger.LogInfo($"Migrating started at {DateTime.Now}");
+        _logger.LogInfo($"Migrating finished at {DateTime.Now}");
+    }
+}
